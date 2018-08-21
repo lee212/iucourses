d0aa253ad5dda9b3511041b2eb83c3dd2b509b5b
Hyungro Lee <hroe.lee@gmail.com>
2018-08-21 14:54:53 -0400

    initial work to create a table

---
+++ examples/cc.sp17.rst
@@ -0,0 +1,47 @@
+.. list-table:: Cloud Computing
+   :widths: 30 10 10 10 10 10
+
+   * - Topic
+     - Source
+     - Length
+     - Type
+     - Chapter
+     - Part
+
+   * - **Introduction To Cloud Computing For Data Intensive Sciences**
+     - `Link <https://drive.google.com/file/d/0B88HKpainTSfN05qc0ZKdGk3UFk/view?usp=sharing>`_
+     - 00:13:13
+     - lecture
+     - 1.1
+     - 1
+   * - **Introduction To Cloud Computing Fundamentals**
+     - `Link <https://drive.google.com/file/d/0B88HKpainTSfSXpXcjVEUmhyTnc/view?usp=sharing>`_
+     - 00:08:31
+     - lecture
+     - 1.1
+     - 2
+   * - **Data Center Model**
+     - `Link <https://drive.google.com/file/d/0B88HKpainTSfTGJKNnAtZzdlS3c/view?usp=sharing>`_
+     - 00:08:08
+     - lecture
+     - 1.1
+     - 3
+   * - **Data Intensive Science**
+     - `Link <https://drive.google.com/file/d/0B88HKpainTSfUU82eHNsRXVRN1k/view?usp=sharing>`_
+     - 00:02:44
+     - lecture
+     - 1.1
+     - 4
+   * - **Iaas, Paas, Saas**
+     - `Link <https://drive.google.com/open?id=0B88HKpainTSfVjVvczI0TWVCbDQ>`_
+     - 00:10:17
+     - lecture
+     - 1.1
+     - 5
+   * - **Cloud Computing Challenges**
+     - `Link <https://drive.google.com/file/d/0B88HKpainTSfTHdiaktfaUpnRjQ/view?usp=sharing>`_
+     - 00:05:27
+     - lecture
+     - 1.1
+     - 6
+
---
+++ generate.py
@@ -0,0 +1,77 @@
+import yaml
+import argparse
+
+class generateRST(object):
+    space = "   "
+
+    def parse_argument(self):
+        parser = argparse.ArgumentParser("generate rst from yaml data")
+        parser.add_argument("-f", dest="fname", help="yaml file")
+        args = parser.parse_args()
+
+        self.args = args
+        self.argparser = parser
+        return args
+
+    def load_yaml(self):
+        with open(self.args.fname) as f:
+            ymldata = yaml.load(f)
+        self.ymldata = ymldata
+        return ymldata
+
+    def gen_table(self, title, header=None, options=None):
+        res = ".. list-table:: %(title)s\n" + \
+                "%(options)s\n" + \
+                "\n" + \
+                "%(header)s\n" + \
+                "%(body)s"
+        self.table_header = header
+        header = self.render_header(header)
+        options = self.render_options(options)
+        body = self.render_body()
+        nres = res % vars()
+        print (nres)
+
+    def render_header(self, header):
+        nheader = ""
+        for h in header:
+            nheader += self.space  + "  - " + h.title() + "\n"
+        header = self.space + "*" + nheader[len(self.space) + 1:]
+        return header
+
+    def render_options(self, options):
+        noptions = ""
+        for opt, val in options.items():
+            noptions += self.space + ":{}: {}".format(opt, val)
+        return noptions
+
+    def render_body(self, body=None):
+        if body:
+            materials = body
+        else:
+            materials = self.ymldata['materials']
+        nbody = ""
+        for material in materials:
+            mbody = ""
+            for header in self.table_header:
+                if header in material:
+                    val = material[header]
+                else:
+                    val = ""
+                if header == "topic":
+                    val = "**" + val.title() + "**"
+                elif header == "source":
+                    if val[:4] == "http":
+                        val = "`Link <{}>`_".format(val)
+                mbody += self.space + "  - {}\n".format(val)
+            nbody += self.space + "*" + mbody[len(self.space) + 1:]
+        return nbody
+
+if __name__ == "__main__":
+    rstgen = generateRST()
+    args = rstgen.parse_argument()
+    rstgen.load_yaml()
+    rstgen.gen_table(title="Cloud Computing", 
+            header=["topic", "source", "length", "type", "chapter", "part"], 
+            options = { "widths": "30 10 10 10 10 10" })
+
